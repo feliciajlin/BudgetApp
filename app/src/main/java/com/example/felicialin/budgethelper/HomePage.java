0488cd080577bc41337a47c31b8a05579ba46686
nguyenjessie <jessienguyen@berkeley.edu>
2017-10-07 13:14:13 -0700

    start of pie

--- app/src/main/java/com/example/felicialin/budgethelper/HomePage.java
+++ app/src/main/java/com/example/felicialin/budgethelper/HomePage.java
@@ -2,6 +2,7 @@ package com.example.felicialin.budgethelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+//        NessieClient client = ;
+//
+//        ArrayList<Entry> yvalues = new ArrayList<Entry>();
+//        yvalues.add(new Entry(8f, 0));
+//        yvalues.add(new Entry(15f, 1));
+//        yvalues.add(new Entry(12f, 2));
+//        yvalues.add(new Entry(25f, 3));
+//        yvalues.add(new Entry(23f, 4));
+//        yvalues.add(new Entry(17f, 5));
+//
+//        PieDataSet dataSet = new PieDataSet(yvalues, "Election Results");
     }
 }
