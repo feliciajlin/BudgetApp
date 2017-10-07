702c8b04c05d4f3b2c6b35405a9d315b8981fe2e
feliciajlin <felicialin@airbears2-10-142-188-152.airbears2.1918.berkeley.edu>
2017-10-07 13:12:59 -0700

    Added buttons to homepage

--- app/src/main/java/com/example/felicialin/budgethelper/HomePage.java
+++ app/src/main/java/com/example/felicialin/budgethelper/HomePage.java
@@ -1,14 +1,47 @@
 package com.example.felicialin.budgethelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HomePage extends AppCompatActivity {
+import com.reimaginebanking.api.nessieandroidsdk.models.Customer;
+import com.reimaginebanking.api.nessieandroidsdk.requestclients.NessieClient;
+
+public class HomePage extends AppCompatActivity implements View.OnClickListener{
+    Button categoryButton;
+    Button historyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        NessieClient client = NessieClient.getInstance("d9d2932feb9206207df39b565750ceb4");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        categoryButton = (Button) findViewById(R.id.category_button);
+        categoryButton.setOnClickListener(this);
+
+        historyButton = (Button) findViewById(R.id.history_button);
+        historyButton.setOnClickListener(this);
+
+        getAccounts(LoginActivity.currentCustomer);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        //categoryButton.setFocusable(!categoryButton.isFocusableInTouchMode());
+        if (view.equals(categoryButton)) {
+            historyButton.setSelected(false);
+            view.setSelected(true);
+        } else {
+            categoryButton.setSelected(false);
+            view.setSelected(true);
+        }
+    }
+
+    public void getAccounts(Customer customer){
 
     }
 }
