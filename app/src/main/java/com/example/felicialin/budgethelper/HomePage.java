fd5343e9591f5fac7ebbe285bd7840d703b3c6c8
nguyenjessie <jessienguyen@berkeley.edu>
2017-10-07 13:27:04 -0700

    fix merge

--- app/src/main/java/com/example/felicialin/budgethelper/HomePage.java
+++ app/src/main/java/com/example/felicialin/budgethelper/HomePage.java
@@ -2,7 +2,6 @@ package com.example.felicialin.budgethelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,17 +9,5 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
-//        NessieClient client = ;
-//
-//        ArrayList<Entry> yvalues = new ArrayList<Entry>();
-//        yvalues.add(new Entry(8f, 0));
-//        yvalues.add(new Entry(15f, 1));
-//        yvalues.add(new Entry(12f, 2));
-//        yvalues.add(new Entry(25f, 3));
-//        yvalues.add(new Entry(23f, 4));
-//        yvalues.add(new Entry(17f, 5));
-//
-//        PieDataSet dataSet = new PieDataSet(yvalues, "Election Results");
     }
 }
