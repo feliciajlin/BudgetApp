1c05e9c3233b6b2b850999039b1f0a19e91eadbf
feliciajlin <felicialin@Felicias-MacBook-Air.local>
2017-10-07 00:16:03 -0700

    First page

--- app/src/main/java/com/example/felicialin/budgethelper/MainActivity.java
+++ app/src/main/java/com/example/felicialin/budgethelper/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.felicialin.budgethelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View getStarted = findViewById(R.id.getStarted);
+        getStarted.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i=new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(i);
     }
 }
